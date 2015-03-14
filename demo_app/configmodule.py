f81de7308316216e26c360530918bafcbc43df0f
tabee <mario.bee@stud.hslu.ch>
2015-03-14 16:59:33 +0100

    classes that overrides and updates the app configuration

--- demo_app/configmodule.py
+++ demo_app/configmodule.py
@@ -4,17 +4,14 @@ class Config(object):
     TESTING = False
     DATABASE_URI = 'sqlite://:memory:'
 
-
 class ProductionConfig(Config):
     CONFIG_VARIANTE = 'Production'
     DATABASE_URI = 'mysql://user@localhost/foo'
-
 
 class DevelopmentConfig(Config):
     CONFIG_VARIANTE = 'Developement'
     HOST = '127.0.0.1'
     DEBUG = True
-
 
 class TestingConfig(Config):
     CONFIG_VARIANTE = 'Testing'
